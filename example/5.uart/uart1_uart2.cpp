05ef711487d7a9e0a8bd4ec71cd326e424279f9f
sxm <sxm@3dant.cn>
2016-11-29 22:46:58 +0800

    init project

---
+++ example/5.uart/uart1_uart2.cpp
@@ -0,0 +1,58 @@
+/*
+file   : *.cpp
+author : shentq
+version: V1.0
+date   : 2015/7/5
+
+Copyright 2015 shentq. All Rights Reserved.
+*/
+
+//STM32 RUN IN eBox
+
+
+#include "ebox.h"
+
+char inputString[1024];
+int i=0;
+
+void uart2_rx_event()
+{
+    uint16_t c;
+    c = uart2.read();
+    uart1.write(c);
+}
+void uart1_rx_event()
+{
+    uint16_t c;
+    c = uart1.read();
+    inputString[i]=c;
+    if (c == '\n')
+    {
+        inputString[i+1] = '\0';
+        uart2.printf(inputString);
+        i=0;
+    }else{
+        i++;
+    }
+}
+void setup()
+{
+    ebox_init();
+    uart1.begin(115200);
+    uart2.begin(115200);
+    uart2.attach(uart2_rx_event,RxIrq);
+    uart1.attach(uart1_rx_event,RxIrq);
+}
+int main(void)
+{
+    setup();
+    uart1.printf("uart1 to uart2 OK!");
+    while(1)
+    {
+        ;
+    }
+}
+
+
+
+
---
+++ mcu/stm32f10x/ebox/ebox_can.h
@@ -0,0 +1,72 @@
+
+#ifndef __CAN_H
+#define __CAN_H
+
+#include "ebox_common.h"
+#include "FunctionPointer.h"
+
+typedef void (*can_irq_handler)(uint32_t id);
+
+typedef enum
+{
+    BSP_CAN_33KBPS = 0,              // 波特率 33K 模式
+    BSP_CAN_50KBPS,                  // 波特率 50K 模式
+    BSP_CAN_625KBPS,                 // 波特率 62.5K 模式
+    BSP_CAN_96KBPS,                  // 波特率 96K 模式
+    BSP_CAN_100KBPS,                 // 波特率 100K 模式
+    BSP_CAN_125KBPS,                 // 波特率 125K 模式
+    BSP_CAN_250KBPS,                 // 波特率 250K 模式
+    BSP_CAN_500KBPS,                 // 波特率 500K 模式
+    BSP_CAN_800KBPS,                 // 波特率 800K 模式
+    BSP_CAN_1MBPS,                   // 波特率 1M模式
+}BSP_CAN_BAUD;
+
+class Can
+{
+    public:
+      	Can(Gpio* p_pin_rx, Gpio* p_pin_tx);
+				void begin(BSP_CAN_BAUD bps);
+				void set_filter(u8 Fifo,u8 nCanType,u8 num,u32 ID,u32 Mask);
+        void interrupt(FunctionalState enable);
+        void attach_interrupt(void (*callback_fun)(void));
+				void set_filter_idlist(u8 nCanType,u32 ID);
+				void set_filter_idmask(u8 nCanType,u8 num,u32 ID,u32 mask);
+				void set_filter_idlist(u8 nCanType,u8 num,u32 ID);
+				u8   write(CanTxMsg *pCanMsg);
+				void   read(CanRxMsg *pCanMsg);
+				u8	 available(void);
+    /** Attach a function to call whenever a serial interrupt is generated
+     *
+     *  @param fptr A pointer to a void function, or 0 to set as none
+     *  @param type Which serial interrupt to attach the member function to (Seriall::RxIrq for receive, TxIrq for transmit buffer empty)
+     */
+    void attach(void (*fptr)(void));
+
+    /** Attach a member function to call whenever a serial interrupt is generated
+     *
+     *  @param tptr pointer to the object to call the member function on
+     *  @param mptr pointer to the member function to be called
+     *  @param type Which serial interrupt to attach the member function to (Seriall::RxIrq for receive, TxIrq for transmit buffer empty)
+     */
+    template<typename T>
+    void attach(T* tptr, void (T::*mptr)(void)) {
+        if((mptr != NULL) && (tptr != NULL)) {
+            _irq.attach(tptr, mptr);
+        }
+    }
+		
+		static void _irq_handler(uint32_t id);    
+    private:
+    	void set_bps(BSP_CAN_BAUD);
+    	
+    private:
+	    CAN_TypeDef* _CANx;
+        Gpio* pin_rx;           //arduino pin number
+        Gpio* pin_tx;           //arduino pin number    
+
+        BSP_CAN_BAUD _bps;  
+protected:
+    FunctionPointer _irq;		
+};
+
+#endif
